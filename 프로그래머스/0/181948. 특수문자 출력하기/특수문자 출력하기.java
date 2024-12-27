c3963a538600a1b231aff68cf5d94bedf2fc896e
Luke_YS <52543606+yjsmk0902@users.noreply.github.com>
2024-12-27 16:50:12 +0900

    [level 0] Title: 특수문자 출력하기, Time: 150.29 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("!@#$%^&*(\\'\"<>?:;\n");
+        
+    }
+}
