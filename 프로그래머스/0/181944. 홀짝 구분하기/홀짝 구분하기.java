d6442d1c631c51ae360da698907b075807e699cf
Luke_YS <52543606+yjsmk0902@users.noreply.github.com>
2024-12-27 16:55:19 +0900

    [level 0] Title: 홀짝 구분하기, Time: 262.59 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n % 2 == 0 ? n + " is even" : n + " is odd");
+
+        br.close();
+    }
+}
