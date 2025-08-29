110a48eeedf07d37a2e24683f277bbe29aaac13f
Luke_YS <52543606+yjsmk0902@users.noreply.github.com>
2025-08-29 10:59:43 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 63.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(n: Int): Int {
+        var answer: Int = 0
+        var temp: Int = n
+        while(temp > 0){
+            answer += temp % 10
+            temp /= 10
+        }
+        return answer
+    }
+}
